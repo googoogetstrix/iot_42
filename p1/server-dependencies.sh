ec468ab664c2a800e06bb078064f5871e147ace8
Boonsom W <bworrawa@student.42.fr>
2026-08-20 13:49:11 +0700

    p1 kinda ready

--- p1/server-dependencies.sh
+++ p1/server-dependencies.sh
@@ -4,13 +4,19 @@ set -e
 apk update
 apk add ca-certificates curl git
 
-curl -sfL https://get.k3s.io | sh -
+
+echo "=== BEFORE K3S ==="
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110" sh -
+echo "=== AFTER K3S ==="
+
 
 mkdir -p /vagrant/shared
 
+echo "=== WAITING FOR node-token TO BE READY  ==="
 while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
     sleep 1
 done
+echo "=== node-token IS READY NOW ==="
 
 cp /var/lib/rancher/k3s/server/node-token /vagrant/shared/node-token
 chmod 644 /vagrant/shared/node-token
